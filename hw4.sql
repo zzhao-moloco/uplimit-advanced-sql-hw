d15e1b835b19a880b576bde23f1755f6823c083b
zzhao-moloco <zeyu.zhao@moloco.com>
2024-05-19 22:48:45 -0700

    Update hw4.sql

--- hw4.sql
+++ hw4.sql
@@ -24,46 +24,48 @@
 -- find urgent, automobile line items from automobile customers by joining customers, orders, lineitems and get row number
 -- after select row number 1,2 and 3 rows, get max key, quantity and part price 
 -- sort order by desc last_order_date and limit to 100
+-- show columns in table orders;
+-- show columns in table lineitem;
 
 WITH automobile_urgent_customer_parts AS (
     SELECT
-        c.c_custkey,
-        o.o_orderkey,
-        l.l_partkey,
-        o.o_orderdate,
-        l.l_extendedprice,
-        l.l_quantity,
-        ROW_NUMBER() OVER (PARTITION BY c.c_custkey ORDER BY l.l_extendedprice DESC) AS price_row_number
-    FROM snowflake_sample_data.tpch_sf1.customer c
-    INNER JOIN snowflake_sample_data.tpch_sf1.orders o
-        ON c.c_custkey = o.o_custkey
-    INNER JOIN snowflake_sample_data.tpch_sf1.lineitem l
-        ON o.o_orderkey = l.l_orderkey
+        c_custkey::string as customer_key,
+        o_orderkey::string as order_key,
+        l_partkey,
+        o_orderdate,
+        l_extendedprice,
+        l_quantity,
+        RANK() OVER (PARTITION BY customer_key ORDER BY l_extendedprice DESC) AS price_rank
+    FROM snowflake_sample_data.tpch_sf1.customer
+    INNER JOIN snowflake_sample_data.tpch_sf1.orders
+        ON customer_key = orders.o_custkey::string
+    INNER JOIN snowflake_sample_data.tpch_sf1.lineitem
+        ON orders.o_orderkey = lineitem.l_orderkey
     WHERE
-        c.c_mktsegment = 'AUTOMOBILE'
-        AND o.o_orderpriority = '1-URGENT'
+        c_mktsegment = 'AUTOMOBILE'
+        AND o_orderpriority = '1-URGENT'
 ),
 top_three_parts AS (
     SELECT
-        c_custkey,
+        customer_key,
         MAX(o_orderdate) AS last_order_date,
-        LISTAGG(o_orderkey, ', ') AS order_numbers,
+        LISTAGG(order_key, ', ') AS order_numbers,
         SUM(l_extendedprice) AS total_spent,
-        MAX(CASE WHEN price_row_number = 1 THEN l_partkey END) AS part_1_key,
-        MAX(CASE WHEN price_row_number = 1 THEN l_quantity END) AS part_1_quantity,
-        MAX(CASE WHEN price_row_number = 1 THEN l_extendedprice END) AS part_1_total_spent,
-        MAX(CASE WHEN price_row_number = 2 THEN l_partkey END) AS part_2_key,
-        MAX(CASE WHEN price_row_number = 2 THEN l_quantity END) AS part_2_quantity,
-        MAX(CASE WHEN price_row_number = 2 THEN l_extendedprice END) AS part_2_total_spent,
-        MAX(CASE WHEN price_row_number = 3 THEN l_partkey END) AS part_3_key,
-        MAX(CASE WHEN price_row_number = 3 THEN l_quantity END) AS part_3_quantity,
-        MAX(CASE WHEN price_row_number = 3 THEN l_extendedprice END) AS part_3_total_spent
+        MAX(CASE WHEN price_rank = 1 THEN l_partkey END) AS part_1_key,
+        MAX(CASE WHEN price_rank = 1 THEN l_quantity END) AS part_1_quantity,
+        MAX(CASE WHEN price_rank = 1 THEN l_extendedprice END) AS part_1_total_spent,
+        MAX(CASE WHEN price_rank = 2 THEN l_partkey END) AS part_2_key,
+        MAX(CASE WHEN price_rank = 2 THEN l_quantity END) AS part_2_quantity,
+        MAX(CASE WHEN price_rank = 2 THEN l_extendedprice END) AS part_2_total_spent,
+        MAX(CASE WHEN price_rank = 3 THEN l_partkey END) AS part_3_key,
+        MAX(CASE WHEN price_rank = 3 THEN l_quantity END) AS part_3_quantity,
+        MAX(CASE WHEN price_rank = 3 THEN l_extendedprice END) AS part_3_total_spent
     FROM automobile_urgent_customer_parts
-    WHERE price_row_number <= 3
-    GROUP BY c_custkey
+    WHERE price_rank <= 3
+    GROUP BY customer_key
 )
 SELECT
-    c_custkey AS C_CUSTKEY,
+    customer_key AS C_CUSTKEY,
     last_order_date AS LAST_ORDER_DATE,
     order_numbers AS ORDER_NUMBERS,
     total_spent AS TOTAL_SPENT,
@@ -77,24 +79,73 @@ SELECT
     part_3_quantity AS PART_3_QUANTITY,
     part_3_total_spent AS PART_3_TOTAL_SPENT
 FROM top_three_parts
-ORDER BY LAST_ORDER_DATE
+ORDER BY LAST_ORDER_DATE DESC
 LIMIT 100;
-
-
-
-
-
-
-
-
-
 
 
 
 -- Review the candidate's tech exercise below, and provide a one-paragraph assessment of the SQL quality. Provide examples/suggestions for improvement if you think the candidate could have chosen a better approach.
 -- Do you agree with the results returned by the query?
-The results did not agree with my query on the c_custkey.
 
+-- The results between my query and candiate query are different. I believe it comes from the candiate not parsing the customer key into string format and the usage of row_number() instead of rank() for computing rank.
 
 -- Is it easy to understand?
+
+-- The candiate query joined top_orders with urgent_orders several times in order to generate the part_1_key, part_2_key and part_3_key. This was not simply to understand.
+
 -- Could the code be more efficient?
+
+-- Yes, it could be more efficient. In CTE producing urgent_orders, parts table was not really needed.
+-- Case when can by used instead of performing multiple joins from top_orders to urgent_orders and using WHERE clause to select the columns with top 3 ranks. The jions are also not using ON clause to filter but instead used WHERE after the join.
+
+-- Candiate solution:
+
+-- with urgent_orders as (
+--     select
+--     	o_orderkey,
+--     	o_orderdate,
+--         c_custkey,
+--         p_partkey,
+--         l_quantity,
+--         l_extendedprice,
+--         row_number() over (partition by c_custkey order by l_extendedprice desc) as price_rank
+--     from snowflake_sample_data.tpch_sf1.orders as o
+--     inner join snowflake_sample_data.tpch_sf1.customer as c on o.o_custkey = c.c_custkey
+--     inner join snowflake_sample_data.tpch_sf1.lineitem as l on o.o_orderkey = l.l_orderkey
+--     inner join snowflake_sample_data.tpch_sf1.part as p on l.l_partkey = p.p_partkey
+--     where c.c_mktsegment = 'AUTOMOBILE'
+--     	and o.o_orderpriority = '1-URGENT'
+--     order by 1, 2),
+
+-- top_orders as (
+--     select
+--     	c_custkey,
+--         max(o_orderdate) as last_order_date,
+--         listagg(o_orderkey, ', ') as order_numbers,
+--         sum(l_extendedprice) as total_spent
+--     from urgent_orders
+--     where price_rank <= 3
+--     group by 1
+--     order by 1)
+
+-- select 
+-- 	t.c_custkey,
+--     t.last_order_date,
+--     t.order_numbers,
+--     t.total_spent,
+--     u.p_partkey as part_1_key,
+--     u.l_quantity as part_1_quantity,
+--     u.l_extendedprice as part_1_total_spent,
+--     u2.p_partkey as part_2_key,
+--     u2.l_quantity as part_2_quantity,
+--     u2.l_extendedprice as part_2_total_spent,
+--     u3.p_partkey as part_3_key,
+--     u3.l_quantity as part_3_quantity,
+--     u3.l_extendedprice as part_3_total_spent
+-- from top_orders as t
+-- inner join urgent_orders as u on t.c_custkey = u.c_custkey
+-- inner join urgent_orders as u2 on t.c_custkey = u2.c_custkey
+-- inner join urgent_orders as u3 on t.c_custkey = u3.c_custkey
+-- where u.price_rank = 1 and u2.price_rank = 2 and u3.price_rank = 3
+-- order by t.last_order_date desc
+-- limit 100
